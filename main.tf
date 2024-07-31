b25e89ce8989a8da7f4f99fb630157da750c370c
lavanya <lavanyaramesh319@gmail.com>
2024-07-31 22:30:06 +0530

    adding listener rule

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnet_ids
+  target_group_arns = [ aws_lb_target_group.main.arn ]
 
   launch_template {
     id      = aws_launch_template.template.id
@@ -54,4 +55,47 @@ resource "aws_autoscaling_group" "asg" {
       value = tag.value
     }
   }
+}
+
+resource "aws_lb_target_group" "main" {
+  name                 = "${var.name}-${var.env}-tg"
+  port                 = var.app_port
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  tags                 = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
+  deregistration_delay = 30
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 5
+    timeout             = 4
+    path                = "/health"
+  }
+
+}
+
+resource "aws_lb_listener_rule" "rule" {
+  listener_arn = var.listener_arn
+  priority     = var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.dns_name]
+    }
+  }
+}
+
+resource "aws_route53_record" "main" {
+  zone_id = var.domain_id
+  name    = local.dns_name
+  type    = "CNAME"
+  ttl     = 30
+  records = [var.lb_dns_name]
 }
